7d118125f87bde973014c4ec65e1dcfee8a3e199
RUNTRO <73249957+RUNTRO@users.noreply.github.com>
2024-03-27 14:07:05 -0400

    Add files via upload

---
+++ code.py
@@ -0,0 +1,20 @@
+import time
+import board
+import pwmio
+from adafruit_motor import servo
+
+# create a PWMOut object on Pin A2.
+pwm = pwmio.PWMOut(board.GP0, duty_cycle=2 ** 15, frequency=50)
+pwm2 = pwmio.PWMOut(board.GP1, duty_cycle=2 ** 15, frequency=50)
+
+# Create a servo object, my_servo.
+my_servo = servo.Servo(pwm)
+my_servo2 = servo.Servo(pwm2)
+
+while True:
+    my_servo.angle = 0
+    my_servo2.angle = 0
+    time.sleep(2)
+    my_servo.angle = 180
+    my_servo2.angle = 180
+    time.sleep(2)
